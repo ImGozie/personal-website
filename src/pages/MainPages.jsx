102d8401638d4beba396c2e5ddbd252ecc2a5692
DESKTOP-L4Q5CDK\Hyperdata 1 <ghoziwkwkwk21@gmail.com>
2024-09-03 13:36:01 +0700

    update

--- src/pages/MainPages.jsx
+++ src/pages/MainPages.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { HiSun } from "react-icons/hi";
+import gsap from 'gsap';
 
 const MainPages = () => {
+  const [theme, setTheme] = useState(false);
+
+  useEffect(() => {
+    if (theme) {
+      gsap.to('#lightTheme', { opacity: 1, height: '100vh', width: '100vh', borderRadius: '0', duration: 0.5 });
+    } else {
+      gsap.to('#lightTheme', { opacity: 0, height: '1', width: '1', borderRadius: '50%', duration: 0.5 });
+    }
+  }, [theme]);
+
   return (
     <div className="w-[90%] h-[90%] bg-black">
         <div className={`h-full grid grid-rows-9 grid-cols-9 gap-2 *:rounded-lg`}>
-            <div className={`bg-slate-200 col-start-2 col-end-7 flex`}>
-              <div className="bg-blue-400 rounded-full"></div>
+            <div className={`col-start-2 col-end-7 flex items-center`}>
+              <button onClick={() => setTheme(!theme)} className="bg-slate-200 rounded-full rounded-br-none text-4xl p-[1%] relative">
+                <HiSun />
+                <div id='lightTheme' className='absolute backdrop-invert h-1 w-1 top-0 left-0 rounded-full'></div>
+              </button>
             </div>
             <div className={`bg-slate-200 col-start-2 col-span-3 row-span-2`}>2</div>
             <div className={`bg-slate-200 row-span-2 col-span-2`}>3</div>
