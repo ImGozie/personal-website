984637825a29923c7c511381c4163ed40fcb6a2e
DESKTOP-L4Q5CDK\Hyperdata 1 <ghoziwkwkwk21@gmail.com>
2024-09-03 11:02:28 +0700

    first commit

--- src/pages/MainPages.jsx
+++ src/pages/MainPages.jsx
@@ -4,10 +4,12 @@ const MainPages = () => {
   return (
     <div className="w-[90%] h-[90%] bg-black">
         <div className={`h-full grid grid-rows-9 grid-cols-9 gap-2 *:rounded-lg`}>
-            <div className={`bg-slate-200 col-start-2 col-end-7`}>1</div>
+            <div className={`bg-slate-200 col-start-2 col-end-7 flex`}>
+              <div className="bg-blue-400 rounded-full"></div>
+            </div>
             <div className={`bg-slate-200 col-start-2 col-span-3 row-span-2`}>2</div>
             <div className={`bg-slate-200 row-span-2 col-span-2`}>3</div>
-            <div className={`bg-slate-200 row-start-1 row-end-5 col-start-7 col-end-9 bg-img-section4`}>4</div>
+            <div className={`bg-slate-200 row-start-1 row-end-5 col-start-7 col-end-9`}>4</div>
             <div className={`bg-slate-200 col-start-2 col-end-4 row-span-2`}>5</div>
             <div className={`bg-slate-200 row-start-4 col-end-7 row-span-3 col-span-3`}>6</div>
             <div className={`bg-slate-200 col-start-7 col-span-2 row-span-2`}>7</div>
