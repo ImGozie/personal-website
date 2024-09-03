7765632b778110a4bc009d61cca4f3c1a9f72e0f
DESKTOP-L4Q5CDK\Hyperdata 1 <ghoziwkwkwk21@gmail.com>
2024-09-03 15:15:56 +0700

    update

--- src/pages/MainPages.jsx
+++ src/pages/MainPages.jsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { HiSun } from "react-icons/hi";
 import { RiMoonClearFill } from "react-icons/ri";
+import Spline from '@splinetool/react-spline';
 import gsap from 'gsap';
 
 const MainPages = () => {
@@ -9,28 +10,21 @@ const MainPages = () => {
   useEffect(() => {
     if (theme) {
       gsap.to('#lightTheme', {
-       opacity: 1, 
-       height: '100vh', 
-       width: '100vw', 
+       opacity: 1,
        duration: 0.5, 
-       borderRadius: 0
       });
     } else {
       gsap.to('#lightTheme', { 
-        opacity: 0, 
-        height: '1', 
-        width: '1', 
+        opacity: 0,
         duration: 0.5, 
-        transformOrigin: "right top", 
-        borderBottomRightRadius: "100%" 
       });
     }
   }, [theme]);
 
   return (
     <>
-      <div id='lightTheme' className='fixed backdrop-invert h-1 w-1 top-0 left-0 rounded-br-[50%] pointer-events-none'></div>
-      <div className="w-[90%] h-[90%] bg-black">
+      <div id='lightTheme' className='fixed w-screen h-screen backdrop-invert pointer-events-none'></div>
+      <div className="w-[90%] h-[95%] bg-black">
           <div className={`h-full grid grid-rows-9 grid-cols-9 gap-2 *:rounded-lg`}>
               <div className={`col-start-2 col-end-7 flex items-center`}>
                 <button onClick={() => setTheme(!theme)} className="bg-slate-200 rounded-full rounded-br-none text-2xl p-[2%]">
@@ -41,7 +35,9 @@ const MainPages = () => {
               </div>
               <div className={`bg-slate-200 col-start-2 col-span-3 row-span-2`}>2</div>
               <div className={`bg-slate-200 row-span-2 col-span-2`}>3</div>
-              <div className={`bg-slate-200 row-start-1 row-end-5 col-start-7 col-end-9`}>4</div>
+              <div className={`bg-slate-200 row-start-1 row-end-5 col-start-7 col-end-9 *:backdrop-invert-0`}>
+                <Spline scene="https://prod.spline.design/lQrls5NlLUcoaYb4/scene.splinecode" />
+              </div>
               <div className={`bg-slate-200 col-start-2 col-end-4 row-span-2`}>5</div>
               <div className={`bg-slate-200 row-start-4 col-end-7 row-span-3 col-span-3`}>6</div>
               <div className={`bg-slate-200 col-start-7 col-span-2 row-span-2`}>7</div>
