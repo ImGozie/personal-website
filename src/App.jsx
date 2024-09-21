e6377d65b6e275cd00e974aa89d26347a26c5bf2
ImGozie <153894673+ImGozie@users.noreply.github.com>
2024-09-21 22:58:32 +0700

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import Navbar from "./components/Navbar"
 import MainPages from "./pages/MainPages"
 
 function App() {
   return (
     <>
-      <div className="flex items-center justify-center bg-zinc-900 w-full h-screen">
-        <MainPages />
-      </div>
+      <Navbar />
+      <MainPages />
+      {/* <div className="flex items-center justify-center bg-black h-screen">
+      </div> */}
       <div className="bg-blue-500 w-full h-screen">
 
       </div>
