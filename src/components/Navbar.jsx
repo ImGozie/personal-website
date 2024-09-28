d63d93baeecd6da691411d16770e35ef18b3371f
ImGozie <153894673+ImGozie@users.noreply.github.com>
2024-09-28 13:35:26 +0700

    up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,26 @@
-import React, { useState } from "react";
-import MenuButton from "./MenuButton";
+import React, { useState, forwardRef } from "react";
+import { Cross as Hamburger } from "hamburger-react";
 
-const Navbar = () => {
+const Navbar = forwardRef(function Navbar(props, ref) {
   const [isOpen, setOpen] = useState(false);
+  console.log(isOpen);
   return (
     <header className="fixed top-0 boxWidth z-10">
       <nav className="w-full flex items-center justify-between sm:px-16 px-6 py-4">
         <h1 className="uppercase">example</h1>
-        <MenuButton isOpen={isOpen} setOpen={setOpen} />
+        <div ref={ref} className="group relative cursor-pointer menu-parrent">
+          <Hamburger
+            easing="ease-out"
+            size={20}
+            toggled={isOpen}
+            toggle={setOpen}
+            rounded
+          />
+          {/* <div className="bounds"></div> */}
+        </div>
       </nav>
     </header>
   );
-};
+});
 
 export default Navbar;
