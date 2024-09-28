cf52297e7edd4971207b5d9a701c7844f52f9731
ImGozie <153894673+ImGozie@users.noreply.github.com>
2024-09-28 08:23:52 +0700

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,17 @@
 import { useEffect, useRef } from "react";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
-import LocomotiveScroll from "locomotive-scroll";
+import Cursor from "./components/Cursor";
 
 function App() {
-  const containerRef = useRef(null);
-
-  useEffect(() => {
-    const scroll = new LocomotiveScroll({
-      el: containerRef.current,
-      smooth: true,
-    });
-    console.log("Locomotive Scroll initialized:", scroll);
-    return () => {
-      scroll.destroy();
-    };
-  }, []);
-
   return (
-    <main data-scroll-container ref={containerRef}>
-      {/* <Navbar /> */}
+    <>
+      <Cursor />
+      <div className={`flexCenter bg-blue-400`}>
+        <Navbar />
+      </div>
       <Hero />
-    </main>
+    </>
   );
 }
 
