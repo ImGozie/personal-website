8fcbc9ffd7eea55881381c7a69f2bb45969a1cc2
ImGozie <153894673+ImGozie@users.noreply.github.com>
2024-09-28 14:41:55 +0700

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,25 @@
 import React, { useState, forwardRef } from "react";
 import { Cross as Hamburger } from "hamburger-react";
+import Magnetic from "../lib/MagneticFX";
 
 const Navbar = forwardRef(function Navbar(props, ref) {
   const [isOpen, setOpen] = useState(false);
   console.log(isOpen);
   return (
     <header className="fixed top-0 boxWidth z-10">
-      <nav className="w-full flex items-center justify-between sm:px-16 px-6 py-4">
+      <nav className="w-full flex items-center justify-between sm:px-16 px-6 py-5">
         <h1 className="uppercase">example</h1>
-        <div ref={ref} className="relative cursor-pointer menu-parrent">
-          <Hamburger
-            easing="ease-out"
-            size={20}
-            toggled={isOpen}
-            toggle={setOpen}
-            rounded
-          />
-          {/* <div className="bounds"></div> */}
-        </div>
+        <Magnetic>
+          <div ref={ref} className="group relative cursor-pointer bounds">
+            <Hamburger
+              easing="ease-out"
+              size={20}
+              toggled={isOpen}
+              toggle={setOpen}
+              rounded
+            />
+          </div>
+        </Magnetic>
       </nav>
     </header>
   );
