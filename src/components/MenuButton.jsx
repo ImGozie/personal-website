f95d9b85894912693dec7ff3d7718b2899e76791
ImGozie <153894673+ImGozie@users.noreply.github.com>
2024-09-28 13:35:24 +0700

    up

--- src/components/MenuButton.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useState } from "react";
-import { Cross as Hamburger } from "hamburger-react";
-
-const MenuButton = ({ isOpen, setOpen }) => {
-  return <Hamburger easing="ease-out" size={30} toggled={isOpen} toggle={setOpen} rounded />;
-};
-
-export default MenuButton;
