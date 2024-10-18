1dafa9b1a74fb8bfdb1d35627a73b97af060a176
ImGozie <153894673+ImGozie@users.noreply.github.com>
2024-10-18 14:26:24 +0700

    navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState, forwardRef } from "react";
 import { Cross as Hamburger } from "hamburger-react";
 import Magnetic from "../lib/MagneticFX";
+import Logo from "../assets/img/loggo.png";
 
 const Navbar = forwardRef(function Navbar(props, ref) {
   const [isOpen, setOpen] = useState(false);
@@ -8,7 +9,7 @@ const Navbar = forwardRef(function Navbar(props, ref) {
   return (
     <header className="fixed top-0 boxWidth z-10">
       <nav className="w-full flex items-center justify-between sm:px-16 px-6 py-5">
-        <h1 className="uppercase">example</h1>
+        <img src={Logo} alt=".Logo" width={100} className="dark:invert filter"/>
         <Magnetic>
           <div ref={ref} className="group relative cursor-pointer bounds">
             <Hamburger
@@ -20,6 +21,7 @@ const Navbar = forwardRef(function Navbar(props, ref) {
             />
           </div>
         </Magnetic>
+        {/* <div className="menu absolute top-0 left-0 bg-slate-400 h-[50vh] w-full"></div> */}
       </nav>
     </header>
   );
