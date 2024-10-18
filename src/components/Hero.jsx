eeee5ae941f6dcc899d3bf236dad0e2864962da5
ImGozie <153894673+ImGozie@users.noreply.github.com>
2024-10-18 18:49:10 +0700

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-    <section className='flex h-screen boxWidth sm:px-16 px-6 py-5 items-center justify-center'>
-      <h3 className='paragraph'>Experienced <span>full-stack developer</span> with a focus on front-end development.</h3>
+    <section className="flex h-screen boxWidth sm:px-16 px-6 py-5 items-center justify-center">
+      <h3 className="paragraph">
+        Experienced
+        <span className="text-[#0B60B0]"> full-stack <span className="!font-['Inter']">developer </span></span>
+        with a focus on
+        <span className="span-decoration"> front-end <span className="!font-['Inter'] text-[#0B60B0] after:content-['.'] after:text-[#F0EDCF]">development</span> </span>
+      </h3>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
